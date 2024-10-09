4b3bd942bbbed60a6bbdd08cee916f441c6dad8c
ddfadela <ddfadela@gmail.com>
2024-10-09 16:53:00 +0100

    chore: separate types into folder

--- src/auth/strategies/accessToken.strategy.ts
+++ src/auth/strategies/accessToken.strategy.ts
@@ -2,11 +2,9 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { JwtPayload } from 'src/common/types/jwt.type';
 
-type JwtPayload = {
-  sub: string;
-  email: string;
-};
+
 @Injectable()
 
 export class AccessTokenStrategy extends  PassportStrategy(Strategy, 'jwt')  {
